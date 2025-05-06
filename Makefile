6097ec4b316ef3e5a20e2713986f3743617d9507
Owen Rumney <owen@owenrumney.co.uk>
2025-05-06 09:25:35 +0100

    feat: add an icon and support done'ing a notification
    - add icon to the app (thanks GPT)
    - add callback to open the notification URL on menu click
      - mark the notification as done when opened
    
    Signed-off-by: Owen Rumney <owen@owenrumney.co.uk>

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
+SHELL=/bin/zsh
+
 .PHONY: activate-env
 activate-env:
 	@echo "Activating the virtual environment"
-	@source .env/bin/activate
+	@. .env/bin/activate
 
 .PHONY: build
-build: 
+build: activate-env
 	@echo "Building a run as an app"
 	@python3 setup.py py2app
 
 .PHONY: install
 install: build
 	@echo "Installing the app"
-	@cp dist/*.app /Applications
+	@rm -rf /Applications/notibar.app
+	@mv dist/*.app /Applications
 	@echo "App installed in /Applications"
--- notibar/github/notifications.py
+++ notibar/github/notifications.py
@@ -18,7 +18,7 @@ def get_github_notifications():
     response = requests.get(
         "https://api.github.com/notifications",
         headers={"Authorization": f"token {token}"},
-        params={"participating": "true"}
+        # params={"participating": "true"}
     )
 
     if response.status_code != 200:
@@ -26,5 +26,48 @@ def get_github_notifications():
         return []
     
     print(f"Fetched {len(response.json())} notifications.")
-    print(response.json())
     return response.json()
+
+def mark_notification_as_read(notification_id):
+    """
+    Mark a notification as read.
+    """
+    token = get_github_token()
+    if not token:
+        print("No GitHub token found.")
+        return False
+    
+    response = requests.patch(
+        f"https://api.github.com/notifications/threads/{notification_id}",
+        headers={"Authorization": f"token {token}"},
+        json={"read": True}
+    )
+
+    if response.status_code != 205:
+        print(f"Error marking notification as read: {response.status_code}")
+        return False
+    
+    print(f"Marked notification {notification_id} as read.")
+    return True
+
+def mark_notification_as_done(notification_id):
+    """
+    Mark a notification as done.
+    """
+    token = get_github_token()
+    if not token:
+        print("No GitHub token found.")
+        return False
+    
+    response = requests.delete(
+        f"https://api.github.com/notifications/threads/{notification_id}",
+        headers={"Authorization": f"token {token}", "Accept": "application/vnd.github+json"},
+
+    )
+
+    if response.status_code != 205:
+        print(f"Error marking notification as done: {response.status_code}")
+        return False
+    
+    print(f"Marked notification {notification_id} as done.")
+    return True
